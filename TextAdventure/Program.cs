2b1904a829a31855a834973f4a1afa9985478467
Benjamin <Benjamin@DeltaEngine.net>
2018-09-21 10:09:13 +0200

    Added compatibility to VS2015

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -6,7 +6,7 @@ namespace TextAdventure
 	{
 		public static void Main()
 		{
-			Console.WriteLine("Welcome to the amazing 4 line text adventure, it will be a short one.");
+			Console.WriteLine("Welcome to the amazing 4 line text adventure, it will be a short one!");
 			Console.WriteLine("You are in a cave. Choose to go 'left' or 'right' to continue");
 			string choice = Console.ReadLine();
 			string outcome = choice == "right"
