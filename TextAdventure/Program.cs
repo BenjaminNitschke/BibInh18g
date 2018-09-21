6f623077957ee3165653a0b1f0befd27cf56e372
Benjamin <Benjamin@DeltaEngine.net>
2018-09-21 10:29:51 +0200

    Useless changes with comments for merging

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -8,7 +8,25 @@ namespace TextAdventure
 		{
 			Console.WriteLine("Welcome to the amazing 4 line text adventure, it will be a short one!");
 			Console.WriteLine("You are in a cave. Choose to go 'left' or 'right' to continue");
+			//Lets add some commands:
+			//Tobias: switch
 			string choice = Console.ReadLine();
+			switch (choice)
+			{
+				case "a":
+					Console.WriteLine("a");
+					break;
+				case "b":
+					Console.WriteLine("b");
+					break;
+
+			}
+			//Thaddaeus: for
+			//Kathy: for
+			//Danny/Jonas: foreach
+			//Jann: while
+			//Imani: do while
+			//Juri: break/continue
 			string outcome = choice == "right"
 				? "Bad choice, a bear is here and kicking your ass. Game Over."
 				: "You found the Treasure, good work. Game Over.";
