2d05e4ec5621ff6f056c4737c207c1823fe2f4b8
Benjamin <Benjamin@DeltaEngine.net>
2018-09-21 09:17:46 +0200

    TextAdventure from first session

---
+++ TextAdventure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TextAdventure
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Welcome to the amazing 4 line text adventure, it will be a short one.");
+			Console.WriteLine("You are in a cave. Choose to go 'left' or 'right' to continue");
+			string choice = Console.ReadLine();
+			string outcome = choice == "right"
+				? "Bad choice, a bear is here and kicking your ass. Game Over."
+				: "You found the Treasure, good work. Game Over.";
+			Console.WriteLine(outcome);
+		}
+	}
+}
---
+++ TextAdventure/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TextAdventure")]
+[assembly: AssemblyCopyright("Copyright © Benjamin Nitschke 2018")]
+[assembly: ComVisible(false)]
+[assembly: Guid("42e67784-b647-4cb1-9d2f-16df7cbd417c")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
