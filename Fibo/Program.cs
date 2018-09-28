d2ecb482b50374eddb1743a4c87e79716dfdd6f5
Danny Low / HBINH18G <Danny.Low@edu.bib.de>
2018-09-28 11:48:00 +0200

    My Fibo Code :)

---
+++ Fibo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Fibo
+{
+
+    public class Program
+    {
+
+        public static int Fibo(int n)
+        {
+
+            int a = 0;
+            int b = 1;
+
+            for (int p = 0; p < n; p++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+        static void Main()
+        {
+            for (int p = 0; p < 50; p++)
+            {
+                Console.WriteLine(Fibo(p));
+            }
+        }
+    }
+}
