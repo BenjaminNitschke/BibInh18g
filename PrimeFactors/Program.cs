bd78f02595d16185404c55f7d8ccd73140bca9c3
Benjamin <Benjamin@DeltaEngine.net>
2018-09-28 11:28:35 +0200

    PrimeFactors TDD example

---
+++ PrimeFactors/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace PrimeFactors
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Assert.That(FindPrimeFactors(2), Is.EqualTo(new[] { 2 }));
+			Assert.That(FindPrimeFactors(4), Is.EqualTo(new[] { 2, 2 }));
+			Assert.That(FindPrimeFactors(6), Is.EqualTo(new[] { 2, 3 }));
+			Assert.That(FindPrimeFactors(10), Is.EqualTo(new[] { 2, 5 }));
+			Assert.That(FindPrimeFactors(37), Is.EqualTo(new[] { 37 }));
+			Assert.That(FindPrimeFactors(32), Is.EqualTo(new[] { 2, 2, 2, 2, 2}));
+		}
+		
+		private static int[] FindPrimeFactors(int number)
+		{
+			List<int> result = new List<int>();
+			while (number % 2 == 0)
+			{
+				result.Add(2);
+				number = number / 2;
+			}
+			if (number > 1)
+				result.Add(number);
+			return result.ToArray();
+		}
+	}
+}
---
+++ PrimeFactors/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PrimeFactors")]
+[assembly: AssemblyCopyright("Copyright © Benjamin 2018")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3b97a79b-f132-4774-80ae-651ebb7033e9")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
