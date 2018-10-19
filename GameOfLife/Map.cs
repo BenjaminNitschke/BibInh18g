82e47c1c5c13e815c1ecd60b561caa8d15dc8ef2
Jann Lüllmann <jann.luellmann@web.de>
2018-10-19 11:05:03 +0200

    Refactored for Rule1

---
+++ GameOfLife/Map.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GameOfLife
+{
+    public class Map
+    {
+        private bool[,] data;
+
+        public Map(int width, int height)
+        {
+            data = new bool[width, height];
+            data[2, 2] = true;
+            data[2, 3] = true;
+            data[3, 2] = true;
+            data[3, 3] = true;
+        }
+
+        public void Update()
+        {
+            data[2, 2] = false;
+        }
+
+        public void Draw()
+        {
+            for (int y = 0; y < this.data.GetLength(1); y++)
+            {
+                for (int x = 0; x < this.data.GetLength(0); x++)
+                {
+                    if (this.data[x, y])
+                        Console.Write(".X");
+                    else
+                        Console.Write(". ");
+
+                    if (x == this.data.GetLength(0) - 1)
+                        Console.Write(".");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool[,] GetData()
+        {
+            return data;
+        }
+    }
+}
--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -5,45 +5,26 @@ namespace GameOfLife
 {
     public class Program
     {
-
-        private bool[,] map;
-
         public static void Main()
         {
             new Program();
         }
 
-        public Program()
-        {
-            Console.WriteLine("Hello World");
-            map = new bool[5, 5];
-
-            Draw(map);
-        }
-
-        public void Draw(bool[,] map)
-        {
-            for (int y = 0; y < this.map.GetLength(1); y++)
-            {
-                for (int x = 0; x < this.map.GetLength(0); x++)
-                {
-                    if (this.map[x, y])
-                        Console.Write(".X");
-                    else
-                        Console.Write(". ");
-
-                    if (x == this.map.GetLength(0) - 1)
-                        Console.Write(".");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        [Test]
+        [Test, Description("Any live cell with fewer than two live neighbors dies, as if by underpopulation")]
         public void Rule1()
         {
-            // Any live cell with fewer htan two live neighbors dies, as if by underpopulation
+            Map map = new Map(5, 5);
+            // Set variables
+            map.GetData()[2, 2] = true;
 
+            // Update game
+            map.Update();
+
+            // Check conditions
+            Assert.That(map.GetData()[2, 2], Is.False);
+
+            // Draw map
+            map.Draw();
         }
     }
 }
