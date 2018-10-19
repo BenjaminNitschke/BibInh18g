5a32a507385f135a136277603f178a14229436f5
Benjamin <Benjamin@DeltaEngine.net>
2018-10-19 09:38:41 +0200

    GameOfLife initial output

---
+++ GameOfLife/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GameOfLife
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Game Of Life");
+			bool[,] map = new bool[40, 20];
+			for (int y = 0; y < map.GetLength(1); y++)
+			{
+				for (int x = 0; x < map.GetLength(0); x++)
+					Console.Write(map[x, y] ? "X" : ".");
+				Console.WriteLine();
+			}
+		}
+	}
+}
---
+++ GameOfLife/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GameOfLife")]
+[assembly: AssemblyCopyright("Copyright © Benjamin Nitschke 2018")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8a58d032-f88f-4929-952e-d6d65cd85c11")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
