85ba7ba32257ac19c19ac588b315728369302325
Jann Lüllmann <jann.luellmann@web.de>
2018-10-19 10:31:02 +0200

    Initial commit

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -1,19 +1,49 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 
 namespace GameOfLife
 {
-	public class Program
-	{
-		public static void Main()
-		{
-			Console.WriteLine("Game Of Life");
-			bool[,] map = new bool[40, 20];
-			for (int y = 0; y < map.GetLength(1); y++)
-			{
-				for (int x = 0; x < map.GetLength(0); x++)
-					Console.Write(map[x, y] ? "X" : ".");
-				Console.WriteLine();
-			}
-		}
-	}
+    public class Program
+    {
+
+        private bool[,] map;
+
+        public static void Main()
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            Console.WriteLine("Hello World");
+            map = new bool[5, 5];
+
+            Draw(map);
+        }
+
+        public void Draw(bool[,] map)
+        {
+            for (int y = 0; y < this.map.GetLength(1); y++)
+            {
+                for (int x = 0; x < this.map.GetLength(0); x++)
+                {
+                    if (this.map[x, y])
+                        Console.Write(".X");
+                    else
+                        Console.Write(". ");
+
+                    if (x == this.map.GetLength(0) - 1)
+                        Console.Write(".");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        [Test]
+        public void Rule1()
+        {
+            // Any live cell with fewer htan two live neighbors dies, as if by underpopulation
+
+        }
+    }
 }
