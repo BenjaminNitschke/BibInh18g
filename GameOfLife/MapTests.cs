09308885e91a3f1774696ddacb53829b9c839288
tobiBarth <43059833+tobiBarth@users.noreply.github.com>
2018-10-23 11:20:17 +0200

    Update GameOfLife/MapTests.cs

--- GameOfLife/MapTests.cs
+++ GameOfLife/MapTests.cs
@@ -33,10 +33,9 @@ namespace GameOfLife
 
             //Assert
             for (int y = 0; y < 3; y++)
-                for (int x = 0; x < 3; x++)
-                {
+                for (int x = 0; x < 3; x++)    
                     Assert.That(map.data[x, y], Is.False);
-                }
+                
 
 
         }
