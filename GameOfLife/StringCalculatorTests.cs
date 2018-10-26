20dbb17c044a70d2a9917e0bba882efda2cd1dbb
Jann Lüllmann <jann.luellmann@web.de>
2018-10-26 08:48:28 +0200

    Added Calculatortest

---
+++ GameOfLife/StringCalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace GameOfLife
+{
+    class StringCalculatorTests
+    {
+        public int Add(string numbers)
+        {
+            //int[] values = numbers.Split('-');
+            return 0;
+        }
+
+
+        [Test, Description("Empty string should return zero")]
+        public void Empty_String()
+        {
+            // Assign
+            string numbers = "";
+            int expected = 0;
+            int result;
+
+            // Act
+            result = Add(numbers);
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+}
