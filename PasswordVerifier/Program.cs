629575da88e2d992488aaa9aa210377db777217b
Jann Lüllmann <jann.luellmann@web.de>
2018-11-02 10:18:50 +0100

    Added PasswordVerifier Project

---
+++ PasswordVerifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace PasswordVerifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        [Test]
+        public void Nothing()
+        {
+
+        }
+    }
+}
