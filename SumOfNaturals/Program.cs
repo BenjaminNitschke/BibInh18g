a375426a4e0f25b80c339b3fd3c2cccc669f41a4
Jann Lüllmann <jann.luellmann@web.de>
2018-12-14 08:43:39 +0100

    Initial commit

---
+++ SumOfNaturals/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SumNaturals
+{
+    class Program
+    {
+        string input;
+        int number;
+        int sum = 0;
+
+        public Program()
+        {
+
+            Console.WriteLine("Please insert a natural number");
+            input = Console.ReadLine();
+
+            if (!Int32.TryParse(input, out number))
+                Console.WriteLine("Please insert a natural number");
+
+            for (int i = 0; i <= number; i++)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                    sum += i;
+            }
+
+            Console.WriteLine("The sum of all natural numbers dividable by 3 and 5 from 0 and " + number + " is " + sum);
+        }
+
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+    }
+}
