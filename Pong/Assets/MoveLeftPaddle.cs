4d84737f0bded77e22f554152e8583fca75b75e9
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2019-01-11 13:39:52 +0100

    Pong Game in Unity from the base up

---
+++ Pong/Assets/MoveLeftPaddle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class MoveLeftPaddle : MonoBehaviour
+{
+	public float Speed = 0.2f;
+
+	public static Rect GetScreenRect()
+	{
+		var ourCamera = Camera.main != null ? Camera.main : Camera.allCameras[0];
+		float halfHeight = ourCamera.orthographicSize;
+		float halfWidth = ourCamera.aspect * halfHeight;
+		return new Rect(-halfWidth, -halfHeight, halfWidth * 2, halfHeight * 2);
+	}
+
+	void FixedUpdate()
+	{
+		float v = Input.GetAxisRaw("Vertical") * Speed;
+		
+		transform.position = new Vector3(GetScreenRect().xMin+0.6f, transform.position.y + v);
+		if (transform.position.y < GetScreenRect().yMin)
+			transform.position = new Vector2(transform.position.x, GetScreenRect().yMin);
+		else if (transform.position.y > GetScreenRect().yMax)
+			transform.position = new Vector2(transform.position.x, GetScreenRect().yMax);
+	}
+}
