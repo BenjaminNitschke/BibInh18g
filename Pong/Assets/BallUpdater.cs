9b165b27d2efdef839102edae74f9ffc9e220a75
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2019-01-18 09:39:14 +0100

    Basic Paddle Physics

--- Pong/Assets/BallUpdater.cs
+++ Pong/Assets/BallUpdater.cs
@@ -3,16 +3,20 @@
 public class BallUpdater : MonoBehaviour
 {
 	public Vector2 velocity = new Vector2(0.09f, 0.06f);
-	private Vector2 size = new Vector2(0.4f, 0.4f);
+	private Vector2 size = new Vector2(0.4f, 0.4f);//transform.localScale isn't very useful
+	public float Left { get { return transform.position.x - size.x / 2; } }
+	public float Right { get { return transform.position.x + size.x / 2; } }
+	public float Top { get { return transform.position.y + size.y / 2; } }
+	public float Bottom { get { return transform.position.y - size.y / 2; } }
 	
 	void FixedUpdate()
 	{
 		transform.position += (Vector3)velocity;
-		if (transform.position.y + size.y / 2 > MoveLeftPaddle.GetScreenRect().yMax ||
-				transform.position.y - size.y / 2 < MoveLeftPaddle.GetScreenRect().yMin)
+		if (Top > MoveLeftPaddle.GetScreenRect().yMax ||
+				Bottom < MoveLeftPaddle.GetScreenRect().yMin)
 			velocity.y *= -1f;
-		if (transform.position.x + size.x / 2 > MoveLeftPaddle.GetScreenRect().xMax ||
-				transform.position.x - size.x / 2 < MoveLeftPaddle.GetScreenRect().xMin)
+		if (Right > MoveLeftPaddle.GetScreenRect().xMax ||
+				Left < MoveLeftPaddle.GetScreenRect().xMin)
 			velocity.x *= -1f;
 	}
 }
--- Pong/Assets/MoveLeftPaddle.cs
+++ Pong/Assets/MoveLeftPaddle.cs
@@ -3,7 +3,22 @@
 [ExecuteInEditMode]
 public class MoveLeftPaddle : MonoBehaviour
 {
+	public GameObject Ball;
+	private BallUpdater ballUpdater;
 	public float Speed = 0.2f;
+	
+	private Vector2 size = new Vector2(0.25f, 1.25f);//transform.localScale isn't very useful
+	public float Left { get { return transform.position.x - size.x / 2; } }
+	public float Right { get { return transform.position.x + size.x / 2; } }
+	public float Top { get { return transform.position.y + size.y / 2; } }
+	public float Bottom { get { return transform.position.y - size.y / 2; } }
+
+	void Start()
+	{
+		if (Ball == null)
+			Ball = GameObject.Find("Ball");
+		ballUpdater = Ball.GetComponent<BallUpdater>();
+	}
 
 	public static Rect GetScreenRect()
 	{
@@ -22,5 +37,19 @@ public class MoveLeftPaddle : MonoBehaviour
 			transform.position = new Vector2(transform.position.x, GetScreenRect().yMin);
 		else if (transform.position.y > GetScreenRect().yMax)
 			transform.position = new Vector2(transform.position.x, GetScreenRect().yMax);
+
+		//Debug.Log("ball left="+ballUpdater.Left+", Paddle right="+Right);
+		if (ballUpdater.Left > Right && ballUpdater.Left + ballUpdater.velocity.x <= Right &&
+				ballUpdater.Bottom < Top && ballUpdater.Top > Bottom)
+		{
+			ballUpdater.velocity.x = Mathf.Abs(ballUpdater.velocity.x);
+			// Where did we hit the paddle, if upper 20% or lower 20% change direction
+			var paddleHitPosition = (Ball.transform.position.y - Bottom) / size.y;
+			//Debug.Log("paddleHitPosition="+paddleHitPosition);
+			if (paddleHitPosition < 0.2f)
+				ballUpdater.velocity.y -= 0.1f;
+			else if (paddleHitPosition > 0.8f)
+				ballUpdater.velocity.y += 0.1f;
+		}
 	}
 }
