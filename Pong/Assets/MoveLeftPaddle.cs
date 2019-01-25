c867fa34ab0b0cb211c64a260e0455e2fc4950be
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2019-01-25 11:24:18 +0100

    Fun with Unity Physics2D

--- Pong/Assets/MoveLeftPaddle.cs
+++ Pong/Assets/MoveLeftPaddle.cs
@@ -28,6 +28,9 @@ public class MoveLeftPaddle : MonoBehaviour
 		return new Rect(-halfWidth, -halfHeight, halfWidth * 2, halfHeight * 2);
 	}
 
+	private int leftScore = 0;
+	private int rightScore = 0;
+
 	void FixedUpdate()
 	{
 		float v = Input.GetAxisRaw("Vertical") * Speed;
@@ -39,17 +42,38 @@ public class MoveLeftPaddle : MonoBehaviour
 			transform.position = new Vector2(transform.position.x, GetScreenRect().yMax);
 
 		//Debug.Log("ball left="+ballUpdater.Left+", Paddle right="+Right);
-		if (ballUpdater.Left > Right && ballUpdater.Left + ballUpdater.velocity.x <= Right &&
-				ballUpdater.Bottom < Top && ballUpdater.Top > Bottom)
+		if (ballUpdater.Left < Left)
 		{
-			ballUpdater.velocity.x = Mathf.Abs(ballUpdater.velocity.x);
-			// Where did we hit the paddle, if upper 20% or lower 20% change direction
-			var paddleHitPosition = (Ball.transform.position.y - Bottom) / size.y;
-			//Debug.Log("paddleHitPosition="+paddleHitPosition);
-			if (paddleHitPosition < 0.2f)
-				ballUpdater.velocity.y -= 0.1f;
-			else if (paddleHitPosition > 0.8f)
-				ballUpdater.velocity.y += 0.1f;
+			if (ballUpdater.Bottom < Top && ballUpdater.Top > Bottom)
+			{
+				/*
+				ballUpdater.velocity.x = Mathf.Abs(ballUpdater.velocity.x);
+				// Where did we hit the paddle, if upper 20% or lower 20% change direction
+				var paddleHitPosition = (Ball.transform.position.y - Bottom) / size.y;
+				//Debug.Log("paddleHitPosition="+paddleHitPosition);
+				if (paddleHitPosition < 0.2f)
+					ballUpdater.velocity.y -= 0.1f;
+				else if (paddleHitPosition > 0.8f)
+					ballUpdater.velocity.y += 0.1f;
+					*/
+			}
+			else
+			{
+				rightScore++;
+				GameObject.Find("Score").GetComponent<UnityEngine.UI.Text>().text =
+					"Score: " + leftScore + " - " + rightScore;
+				ballUpdater.transform.position = new Vector3(0, 0);
+				var rigidBody = ballUpdater.GetComponent<Rigidbody2D>();
+				rigidBody.velocity= new Vector2(0, 0);
+				rigidBody.inertia = 0;
+				rigidBody.AddForce(new Vector2(Random.Range(0, 2) == 0 ? -5 : 5, Random.Range(-3.0f, 3.0f)));
+			}
 		}
+
+		//simple ai
+		GameObject.Find("RightPaddle").transform.position = new Vector3(GetScreenRect().xMax-0.6f, ballUpdater.transform.position.y);
+		if (ballUpdater.Right > GameObject.Find("RightPaddle").transform.position.x)
+			leftScore++;
+
 	}
 }
