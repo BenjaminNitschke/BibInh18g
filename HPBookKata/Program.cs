2c963154f72c844823688db6929483ba8ec137f6
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2019-02-15 11:02:50 +0100

    Code Review

--- HPBookKata/Program.cs
+++ HPBookKata/Program.cs
@@ -4,59 +4,60 @@ using NUnit.Framework;
 
 namespace HPBookKata
 {
-    class Program
-    {
-        public decimal price;
-        public List<string> buy= new List<string>();
+	class Program
+	{
+		static void Main()
+		{
+			new Program().Buy();
+		}
 
-        static void Main()
-        {
-            Program asdf = new Program();
-            asdf.Buy();
-        }
+		public void Buy()
+		{
+			Console.WriteLine("Book? (y) (n)");
+			string yn = Console.ReadLine();
 
-        public void Buy()
-        {
-            Console.WriteLine("Book? (y) (n)");
-            string yn = Console.ReadLine();
+			if (yn == "n")
+			{
+				BookCalculator(booksToBuy);
+				return;
+			}
 
-            if (yn == "n")
-            {
-                BookCalculator(buy);
-            }
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine("Book " + (i + 1) + " ?:");
+				booksToBuy.Add(int.Parse(Console.ReadLine()));
+			}
 
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Book "+ (i+1) +" ?:");
-                buy.Add(Console.ReadLine());
-            }
-            BookCalculator(buy);
-        }
-        
-        public decimal BookCalculator(List<string>buy)
-        {
-            if (buy.Count > 1)
-            {
+			BookCalculator(booksToBuy);
+		}
 
-            }
-            else if (buy.Count == 0)
-            {
-                Nothing();
-            }
-            else if (buy.Count == 1)
-            {
-                BuyOneBook();
-            }
-            return price;
-        }
-        public void Nothing()
-        {
-            Console.WriteLine("No book");
-            Environment.Exit(-1);
-        }
-        public void BuyOneBook()
-        {
-            Console.Write("OneBook = 8€");
-        }
-    }
+		public List<int> booksToBuy = new List<int>();
+		
+		public void BookCalculator(List<int> buy)
+		{
+			if (buy.Count == 0)
+				Nothing();
+			else if (buy.Count == 1)
+				BuyOneBook();
+			else if (buy.Count > 1)
+				BuyMultipleBooks();
+		}
+
+		public void Nothing()
+		{
+			Console.WriteLine("No book");
+		}
+
+		public void BuyOneBook()
+		{
+			Console.Write("OneBook = 8€");
+		}
+
+		private void BuyMultipleBooks()
+		{
+			Console.Write("OneBook = 8€");
+
+		}
+
+	}
 }
