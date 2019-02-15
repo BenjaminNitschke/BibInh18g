d67d978d0f2e08a5f01dad934ae882c2548a91ea
Jann Lüllmann <jann.luellmann@web.de>
2019-02-15 09:53:12 +0100

    Initial Commit

---
+++ PotterKata/BookCalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterKata
+{
+    class BookCalculator
+    {
+        private int[] availableBooks = new int[]{ 1, 2, 3, 4, 5 };
+
+        private Dictionary<int, decimal> discounts = new Dictionary<int, decimal> {
+            {0, 0m },
+            {1, 0m },
+            {2, 0.05m },
+            {3, 0.1m },
+            {4, 0.2m },
+            {5, 0.3m }
+        };
+
+        private decimal pricePerBook = 8.0m;
+        
+        public decimal Calculate(params int[] books)
+        {
+            decimal price = 0;
+
+            //Array.ForEach(books, book => )
+
+            List<int> kindOfBooks = new List<int>();
+            for (int i = 0; i < books.Length; i++)
+            {
+                if (!kindOfBooks.Contains(books[i]))
+                    kindOfBooks.Add(books[i]);
+            }
+
+            for (int i = 0; i < books.Length; i++)
+            {
+                price += pricePerBook;
+            }
+
+            if (kindOfBooks.Count <= 5)
+                price *= 1 - discounts[kindOfBooks.Count];
+            else
+                price *= 1 - discounts[discounts.Count-1];
+
+            return price;
+        }
+    }
+}
---
+++ PotterKata/Program.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterKata
+{
+    class Program
+    {
+        private BookCalculator calculator;
+        private decimal pricePerBook = 8m;
+
+        public Program()
+        {
+            calculator = new BookCalculator();
+        }
+
+        [Test]
+        public void Nothing()
+        {
+        }
+
+        [Test]
+        public void BuyNoBook()
+        {
+            Assert.That(calculator.Calculate(), Is.EqualTo(0));
+        }
+
+        [TestCase(0)]
+        [TestCase(6)]
+        public void BuyWrongBook(int bookNumber)
+        {
+            Assert.That(calculator.Calculate(bookNumber), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void BuyOneBook()
+        {
+            Assert.That(calculator.Calculate(1), Is.EqualTo(8));
+        }
+
+        [TestCase(1, 1)]
+        [TestCase(3, 3, 3)]
+        [TestCase(5, 5, 5, 5, 5)]
+        public void BuySameBooks(params int[] numbers)
+        {
+            // Arange
+            BookCalculator calculator = new BookCalculator();
+            decimal targetPrice = numbers.Length * pricePerBook;
+
+            // Act
+            decimal result = calculator.Calculate(numbers);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(targetPrice));
+        }
+
+        [TestCase(1, 2)]
+        [TestCase(5, 3)]
+        [TestCase(1, 4)]
+        // Two different books give a 5% discount
+        public void BuyTwoDifferentBooks(params int[] numbers)
+        {
+            // Arange
+            BookCalculator calculator = new BookCalculator();
+            decimal targetPrice = 2m * 8m * 0.95m;
+
+            // Act
+            decimal result = calculator.Calculate(numbers);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(targetPrice));
+        }
+
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+    }
+}
