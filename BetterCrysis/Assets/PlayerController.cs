10186b7dd33e2df4e46c2eb825117363dfeea786
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2019-03-08 11:23:08 +0100

    BetterCrysis Unity 3D project with camera, player controller and all that fun

---
+++ BetterCrysis/Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	void Update ()
+	{
+		transform.position += MoveSpeed * transform.forward * Input.GetAxis("Vertical") * Time.deltaTime;
+		transform.Rotate(0, RotateSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0);
+	}
+
+	private const float MoveSpeed = 5.0f;
+	private const float RotateSpeed = 180f;
+}
