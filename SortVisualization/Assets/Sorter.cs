2ec173ef5ba1dc784850a1a5d7bc65bb82b4de71
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2019-05-10 09:31:34 +0200

    QuickSort recap and implementation shown again with yield

--- SortVisualization/Assets/Sorter.cs
+++ SortVisualization/Assets/Sorter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 // ReSharper disable once UnusedMember.Global
@@ -12,54 +13,68 @@ public class Sorter : MonoBehaviour
 		compareMaterial = Resources.Load<Material>("compareMaterial");
 		limitMaterial = Resources.Load<Material>("limitMaterial");
 		smallestMaterial = Resources.Load<Material>("smallestMaterial");
-		StartCoroutine("SortAll");
+		swapMaterial = Resources.Load<Material>("swapMaterial");
+		StartCoroutine(QuickSort(0, list.Length - 1));
 	}
-
+	
 	private MeshRenderer[] list;
-	private int iteration;
 	private Material defaultMaterial;
 	private Material compareMaterial;
 	private Material limitMaterial;
 	private Material smallestMaterial;
-
-	private IEnumerator SortAll()
+	private Material swapMaterial;
+	
+	/// <summary>
+	/// https://en.wikipedia.org/wiki/Quicksort
+	/// </summary>
+	private IEnumerator QuickSort(int lo, int hi)
 	{
-		for (iteration = list.Length-1; iteration >= 0; iteration--)
+		if (lo >= hi)
+			yield break;
+		int p = lo;
+		yield return Partition(lo, hi, result => p = result);
+		yield return QuickSort(lo, p - 1);
+		yield return QuickSort(p + 1, hi);
+	}
+
+	private IEnumerator Partition(int lo, int hi, Action<int> result)
+	{
+		float pivot = list[hi].transform.localScale.x;
+		int i = lo;
+		for (int j = lo; j < hi; j++)
 		{
-			var biggest = list[iteration].transform.localScale.x;
-			var biggestIndex = iteration;
-			for (int i = 0; i < iteration; i++)
+			SetPositionAndMaterialOfAllSortedGameObjects(lo, j, hi);
+			if (list[j].transform.localScale.x < pivot)
 			{
-				if (list[i].transform.localScale.x > biggest)
-				{
-					biggest = list[i].transform.localScale.x;
-					biggestIndex = i;
-				}
-				SetPositionAndMaterialOfAllSortedGameObjects(i, biggestIndex);
-				yield return new WaitForSeconds(0.01f);
+				Swap(list, i, j);
+				list[i].material = swapMaterial;
+				i++;
 			}
-			if (biggestIndex != iteration)
-				Swap(list, biggestIndex, iteration);
+			yield return new WaitForSeconds(1f);
 		}
+		Swap(list, i, hi);
+		SetPositionAndMaterialOfAllSortedGameObjects(lo, i, hi);
+		yield return new WaitForSeconds(1f);
+		result(i);
 	}
 	
-	private void SetPositionAndMaterialOfAllSortedGameObjects(int currentIndex, int smallestIndex)
+	private void SetPositionAndMaterialOfAllSortedGameObjects(int lo, int currentIndex, int hi)
 	{
 		for (int i = 0; i < list.Length; i++)
 		{
 			list[i].transform.position = i * new Vector3(3, 0, 0);
-			if (i == iteration)
+			if (i == lo)
 				list[i].material = limitMaterial;
 			else if (i == currentIndex)
 				list[i].material = compareMaterial;
-			else if (i == smallestIndex)
+			else if (i == hi)
 				list[i].material = smallestMaterial;
 			else
 				list[i].material = defaultMaterial;
 		}
 	}
 	
-	private static void Swap(MeshRenderer[] list, int first, int second)
+	private static void Swap<T>(T[] list, int first, int second)
 	{
 		var rememberValue = list[first];
 		list[first] = list[second];
