5c4911f7002d0ac5b61fcf409bfb63aaafb68c93
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2019-05-24 11:13:12 +0200

    merged with jonas and checked in working version of credits, options still work in progress

--- BetterCrysis/Assets/Scripts/Menu/CreditsState.cs
+++ BetterCrysis/Assets/Scripts/Menu/CreditsState.cs
@@ -1,7 +1,12 @@
-﻿public class CreditsState : MenuState
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsState : MenuState
 {
 	public override void Execute()
-    {
-        
-    }
+	{
+		var operation = SceneManager.UnloadSceneAsync("MenuScene");
+		operation.completed +=
+			asyncOperation => SceneManager.LoadScene("Credits", LoadSceneMode.Additive);
+	}
 }
