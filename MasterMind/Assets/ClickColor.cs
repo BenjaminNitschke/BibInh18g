936eadab3ec15f5c79352c9de3142abb85eeaf2d
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2019-07-05 13:05:39 +0200

    First pick color now possible

---
+++ MasterMind/Assets/ClickColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ClickColor : MonoBehaviour, IPointerClickHandler {
+	private ColorManager colorManager;
+
+	// Use this for initialization
+	void Start ()
+	{
+		colorManager = GameObject.Find("Shield").GetComponent<ColorManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		colorManager.SetNextColor(GetComponent<Image>().color);
+	}
+}
---
+++ MasterMind/Assets/ColorManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorManager : MonoBehaviour {
+	private GameObject[] picks;
+	private GameObject[] pins;
+
+	// Use this for initialization
+	void Start ()
+	{
+		picks = new[]
+		{
+			GameObject.Find("Pick1Position"), GameObject.Find("Pick1Position"),
+			GameObject.Find("Pick1Position"), GameObject.Find("Pick1Position")
+		};
+		pins = new[]
+		{
+			GameObject.Find("Pin1Position"), GameObject.Find("Pin1Position"),
+			GameObject.Find("Pin1Position"), GameObject.Find("Pin1Position")
+		};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetNextColor(Color color)
+	{
+		//Todo: number
+		var image = picks[0].GetComponent<Image>();
+		image.enabled = true;
+		image.color = color;
+	}
+}
